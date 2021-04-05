9fc36cd0797b1d032187fb3b7bd0351e01dc3a57
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-04-05 17:58:59 -0500

    Concatenación

---
+++ JS/01 Tipos de Datos/concatenacion&usoDeVariables.js
@@ -0,0 +1,14 @@
+var name = 'Kalel',
+apellido = 'Kent';
+
+var completeName = name + ' ' + apellido;
+console.log(completeName);
+
+var x = name+219;
+console.log(typeof x);
+
+x = name + (12+6);
+console.log(x);
+
+x = 2+8 + name;
+console.log(x);
