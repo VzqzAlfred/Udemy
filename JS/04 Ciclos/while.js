b9bfa814ed49a94a8738548e9a90ccb6d67880bf
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-04-10 13:24:31 -0500

    Clo while

---
+++ JS/04 Ciclos/while.js
@@ -0,0 +1,16 @@
+/* While */
+let count = 0;
+while(count<=5){
+    console.log(count);
+    count++;
+}
+console.log("\n");
+
+
+
+/* Do while */
+count = 0;
+do {
+    console.log(count);
+    count++;
+} while (count <=2);
