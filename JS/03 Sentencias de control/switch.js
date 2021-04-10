e5ac3a66c7db50a601f229606fdd36de9d334054
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-04-09 19:31:18 -0500

    Uso de switch

---
+++ JS/03 Sentencias de control/switch.js
@@ -0,0 +1,28 @@
+let num = 10;
+
+let numText = 'Value strange';
+
+switch (num) {
+    case 1:
+        numText="Number one"
+        console.log(numText);
+        break;
+
+    case 2:
+        numText="Number two"
+        console.log(numText);
+        break;
+    
+    case 3:
+        numText="Number three"
+        console.log(numText);
+        break;
+    
+    case 4:
+        numText="Number four"
+        console.log(numText);
+            break;
+    default:
+        console.log(numText);
+        break;
+}
