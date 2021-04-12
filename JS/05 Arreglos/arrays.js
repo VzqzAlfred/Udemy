09ee7b29a56a70d260b4a1a55360f46271ef9db3
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-04-12 17:11:29 -0500

    Manejo de Arreglos

--- JS/05 Arreglos/arrays.js
+++ JS/05 Arreglos/arrays.js
@@ -1,5 +1,6 @@
 let array = [1,2,"Hola",[10,20,30], 'un string'];
 
+console.log(array);
 console.log(array[3]);
 console.log(array[3][2]);
 
@@ -7,6 +8,29 @@ let autos = Array(100).fill("Car");
 console.log(autos);
 console.log(autos.length);
 
-autos.forEach((el, i) => {
-    console.log(`${i}.- ${el}`);
-})
+
+
+const cities = new Array("CDMX", "New York", "Oslo", "Masachuttess");
+console.log(cities);
+
+for(let i = 0; i < cities.length; i++){
+    console.log(`Value ${i}: '${cities[i]}'`);
+}
+
+/* Modify arrays */
+cities[3] = "Estocolmo";
+console.log(cities);
+cities.push("Swedan");
+console.log(cities);
+
+
+cities[cities.length] = "Mosku";
+console.log(cities);
+
+cities[8] = "Tokyo"
+console.log(cities);
+
+
+console.log(typeof(array));
+console.log(Array.isArray(array));
+console.log(array instanceof Array);
