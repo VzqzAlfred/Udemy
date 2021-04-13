543b83dc3f1ee86642589d70abf49572c109c32d
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-04-13 14:22:02 -0500

    Manejo de Funciones

--- JS/06 Funciones/functions.js
+++ JS/06 Funciones/functions.js
@@ -1,8 +1,40 @@
-const date = new Date().toLocaleTimeString();
-
 function myF(a,b){
-    return console.log(a+b);
+    let c = a + b;
+    console.log(arguments);
+    console.log(`Cantidad:` ,arguments.length);
+    return console.log(c);
 }
 myF(5,5);
 
-console.log(date);
+
+
+/* Funciones de Tipo expresión */
+let x = function(a,b){
+    return a+b;
+};
+let res = x(10,25);
+console.log(res); 
+
+
+
+/* Funciones autoejecutadas */
+(function(a,b){
+    console.log("Ejecutando una función autoejecutable", a+b);
+})(3,5);
+
+
+
+/* Funciones como objetos */
+var miFuncionTexto = myF.toString();
+console.log(miFuncionTexto);
+console.log(typeof(miFuncionTexto));
+
+
+
+/* Funciones flecha */
+const resta = (x,y) => {
+    let res = x - y;
+    return res;
+}
+res = resta(18,6);
+console.log(res);
