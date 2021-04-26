02d990ae2bd8772234f63ab0f62c6624d6c14260
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-04-26 14:38:32 -0500

    Uso del modo estricto en JS

---
+++ JS/08 Clases/modoStrict.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*Se declara d esa manera y siempre al comeinzo de un programa hace que las variables siempre esten declaras */
+
+
+// x = 10; con el modo estricto no funciona, sin el use strict funciona
+let x = 10; 
+console.log(x);
+
+myFunction();
+
+function myFunction(){
+    y = 8;
+    console.log(y);
+}
