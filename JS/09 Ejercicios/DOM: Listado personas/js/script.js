1d5cb27b3698ce5f438f9822ee5fe9f869843a5d
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-05-14 19:24:49 -0500

    Ejercicio: Listado de personas

--- JS/09 Ejercicios/DOM: Listado personas/js/script.js
+++ JS/09 Ejercicios/DOM: Listado personas/js/script.js
@@ -1,16 +1,30 @@
 const personas = [
-    new Persona('Juan', 'Perez'),
-    new Persona('Billy', 'Buttler')
+  new Persona("Juan", "Perez"),
+  new Persona("Billy", "Buttler"),
 ];
 
-function mostrarPersonas(){
-    console.log("Show person...");
+function mostrarPersonas() {
+  console.log("Show person...");
 
-    let texto = '';
+  let texto = "";
 
-    for (let persona of personas) {
-        
-        texto += `<li>${persona._nombre} ${persona._apellido}</li>`;
-    }
-    document.getElementById("personas").innerHTML = texto;
+  for (let persona of personas) {
+    texto += `<li>${persona._nombre} ${persona._apellido}</li>`;
+  }
+  document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona() {
+  const forma = document.forms["forma"],
+    nombre = forma["nombre"],
+    apellido = forma["apellido"];
+
+  if (nombre.value != "" && apellido.value != "") {
+    const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    mostrarPersonas();
+  }else{
+      alert("Los campos no deben estar vacíos.")
+  }
 }
