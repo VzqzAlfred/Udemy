1da052f9f47f0a10f4edf1de312025aac7585b57
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-05-19 00:54:53 -0500

    Ejercicio: Lista de presupuestos

---
+++ JS/09 Ejercicios/DOM: Presupuesto/js/app.js
@@ -0,0 +1,18 @@
+const ingresos = [
+    new Ingreso('Salario', '2100.00'),
+    new Ingreso('Venta coche', 1500)
+];
+
+const egresos = [
+    new Egreso('Renta', 900),
+    new Egreso('Ropa', 400)
+];
+
+
+const app = () => {
+    cargarCabecer();
+}
+
+const cargarCabecero = () => {
+    
+}
