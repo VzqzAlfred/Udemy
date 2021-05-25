30107848047f090bbbc70ef91d37d8ec8a4828d2
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-05-24 23:14:30 -0500

    Definición de tipos de variables

---
+++ TypeScript/TS-1/02 Definición de tipos de datos/funciones.js
@@ -0,0 +1,13 @@
+"use strict";
+var saludo = "Hi with TypeScript";
+saludo = "Cambio de nombre";
+var numero = 5;
+var cualquiera;
+cualquiera = true;
+function greeting() {
+    console.log("Hi everyone :)");
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+}
+greeting();
---
+++ TypeScript/TS-1/02 Definición de tipos de datos/funciones.ts
@@ -0,0 +1,20 @@
+let saludo:string = "Hi with TypeScript";
+saludo="Cambio de nombre";
+
+
+let numero:number = 5;
+
+
+let cualquiera:any;
+cualquiera = true;
+
+
+function greeting(){
+    console.log("Hi everyone :)");
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+    
+}
+
+greeting();
