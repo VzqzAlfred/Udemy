055d0007e21f02fbcf4826460af3257737a6e469
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-05-26 19:25:45 -0500

    Módulos en TS

---
+++ TypeScript/TS-1/06 Módulos/persona.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Persona = void 0;
+var Persona = /** @class */ (function () {
+    function Persona(nombre) {
+        this.nombre = nombre;
+    }
+    return Persona;
+}());
+exports.Persona = Persona;
---
+++ TypeScript/TS-1/06 Módulos/persona.ts
@@ -0,0 +1,6 @@
+export class Persona{
+    nombre:string;
+    constructor(nombre:string){
+        this.nombre = nombre;
+    }
+}
