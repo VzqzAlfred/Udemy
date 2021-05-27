d06f93aaf64a73d4366857544dd8fb0884a9055f
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-05-26 19:25:09 -0500

    Módulos en TS

---
+++ TypeScript/TS-1/06 Módulos/modulos.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var persona_1 = require("./persona");
+var person1 = new persona_1.Persona("Billy");
+console.log(person1.nombre);
---
+++ TypeScript/TS-1/06 Módulos/modulos.ts
@@ -0,0 +1,5 @@
+import {Persona} from './persona';
+
+let person1 = new Persona("Billy");
+
+console.log(person1.nombre);
