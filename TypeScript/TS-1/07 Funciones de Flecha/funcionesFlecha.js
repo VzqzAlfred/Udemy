980113b8f3fea58b4e4e07c85a8dce2e98b35272
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-05-26 19:58:10 -0500

    Manejo de Arrow Function

---
+++ TypeScript/TS-1/07 Funciones de Flecha/funcionesFlecha.js
@@ -0,0 +1,13 @@
+"use strict";
+var suma = function (a, b) {
+    return a + b;
+};
+console.log(suma(5, 3));
+var sumaFlecha = function (a, b) { return a + b; };
+console.log(sumaFlecha(10, 52));
+var getName = function () {
+    return "Billy Blutter";
+};
+console.log(getName());
+var getNameArrow = function () { return "BILLY BLUTER"; };
+console.log(getNameArrow());
---
+++ TypeScript/TS-1/07 Funciones de Flecha/funcionesFlecha.ts
@@ -0,0 +1,18 @@
+let suma = function(a:number, b:number){
+    return a+b;
+}
+console.log(suma(5,3));
+
+
+let sumaFlecha = (a:number, b:number) => a+b;
+console.log(sumaFlecha(10,52));
+
+
+var getName = function(){
+    return "Billy Blutter";
+}
+console.log(getName());
+
+
+let getNameArrow = () => "BILLY BLUTER";
+console.log(getNameArrow());
