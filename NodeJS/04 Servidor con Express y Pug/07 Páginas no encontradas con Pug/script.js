7bf0256e454e7743a7e36e57a1e97562334e75ae
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-07-26 14:26:41 -0500

    Páginas de error con Pug

---
+++ NodeJS/04 Servidor con Express y Pug/07 Páginas no encontradas con Pug/script.js
@@ -0,0 +1,66 @@
+var express = require('express'),
+    app = express(),
+    pug = require('pug');
+
+app.use(express.static(__dirname+"/public"));
+
+var enemies = [
+    {
+        name: "Ra's Al Ghul",
+        author: "Dennis O'Neil",
+        image: "ras.jpg"
+    },
+    {
+        name: "The Joker",
+        author: "Bill Finger",
+        image: "joker.jpg"
+    },
+    {
+        name: "Harley Quinn",
+        author: "Paul Dini",
+        image: "harley.jpg"
+    },
+    {
+        name: "Bane",
+        author: "Graham Nolan",
+        image: "bane.jpg"
+    }
+];
+
+app.get("/", (req, res) => {
+    // res.send("index.html");
+    res.render('index.pug', {
+        title: "Welcome Batman to Arkham City!",
+        text: "Select an enemies from Arkham City",
+        image: "batman.jpg",
+        villains: enemies
+    });
+});
+
+app.get("/enemy/:name", (req, res) => {
+
+    var dataEnemy  = enemies.filter(enemy => {
+        if (req.params.name === enemy.name) {
+            return enemy;
+        }
+    })[0];
+
+    res.render('enemy.pug', {
+        name: req.params.name,
+        data: dataEnemy
+    });
+});
+
+app.use((req, res) => {
+    res.status(400);
+
+    let err = req.originalUrl;
+
+    res.render("404.pug", {
+        text: err
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
