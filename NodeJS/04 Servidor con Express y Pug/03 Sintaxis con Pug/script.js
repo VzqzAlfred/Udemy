a2595f99a0c3e77e29137c77331c15494b969092
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-07-26 12:53:53 -0500

    Sintaxis con Pug

---
+++ NodeJS/04 Servidor con Express y Pug/03 Sintaxis con Pug/script.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+    app = express(),
+    pug = require('pug');
+
+app.use(express.static(__dirname+"/public"))
+
+app.get("/", (req, res) => {
+    // res.send("index.html");
+    res.render('index.pug');
+});
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
