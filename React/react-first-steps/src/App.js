83b4c4a22f982db762ecf1da76b94ee7af3584df
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-09-13 15:37:24 -0500

    First Steps

---
+++ React/react-first-steps/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+const greeting = name => {
+     let present = <h3>Hi {name}</h3>;
+     return present;
+}
+
+
+function App() {
+     
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          {
+               greeting("user")
+          }
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
