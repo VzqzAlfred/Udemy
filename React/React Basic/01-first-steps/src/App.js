d074c409d067d6a80dcc1187388eabc3fe48a9cb
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-09-23 16:35:23 -0500

    Feedback

--- React/React Basic/01-first-steps/src/App.js
+++ React/React Basic/01-first-steps/src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 import { Component1 } from './Components/Component1';
 import { Greeting } from './Components/Greeting';
 
+
 function App() {
+
+     const userName = "Anonymous";
+     const height = 73;
+
+     const user = {
+          name: "user Anonymous",
+          height: 193,
+          color: "Purple"
+     }
+
+     const Saludar = name => {
+          alert( `Hi ${name}` );
+     }
+     
 
   return (
     <div className="App">
@@ -16,7 +31,9 @@ function App() {
         <Component1 />
 
 
-        <Greeting name="Buddy"  height="168"/>
+        {/* <Greeting name="Buddy"  height="168"/> */}
+        <Greeting user={user} Saludar={Saludar} />
+
       </header>
     </div>
   );
--- React/React Basic/01-first-steps/src/Components/Greeting.jsx
+++ React/React Basic/01-first-steps/src/Components/Greeting.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-export const Greeting = (props) => {
+export const Greeting = ({user, Saludar}) => {
 
-     console.log(props);
+     // console.log(props);
+
+     const {name, height, color} = user;
+     
      return (
           <>
-               <h2>Hi {props.name} my height is {props.height} cm.</h2>
+               {/* <h2>Hi {props.name}, my height is {props.height} cm.</h2> */}
+               <h2>Hi {user.name}, my height is {user.height} cm. And my favorite color is: {user.color}</h2>
+
+               <button onClick={ ()=> Saludar(name) }>Click me :D</button>
           </>
      )
 }
