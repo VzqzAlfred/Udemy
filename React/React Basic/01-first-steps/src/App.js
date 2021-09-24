b44b109d534dd686d625178f21ee11d9c34082f6
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-09-24 15:21:10 -0500

    Feedback

--- React/React Basic/01-first-steps/src/App.js
+++ React/React Basic/01-first-steps/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Component1 } from './Components/Component1';
 import { Greeting } from './Components/Greeting';
+import { Alert, Button} from 'react-bootstrap';
+import {ReactComponent as Batman} from "./assets/bat.svg"
 
 
 function App() {
@@ -21,6 +23,7 @@ function App() {
      
 
   return (
+     <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -30,12 +33,30 @@ function App() {
 
         <Component1 />
 
-
         {/* <Greeting name="Buddy"  height="168"/> */}
         <Greeting user={user} Saludar={Saludar} />
 
+
+     <hr />
+
+     <div>
+          <h3>React Bootstrap</h3>
+          <Button variant="success" onClick={() => alert("Testing bootstrap")}>Botonazo</Button>
+          <br />
+          <br />
+          <Alert variant="success">
+               This is a success alert with{' '}
+          <Alert.Link>an example link</Alert.Link>. Give it a click if you
+    like.
+  </Alert>
+          
+
+          <Batman />
+
+     </div>
       </header>
     </div>
+    </>
   );
 }
 
