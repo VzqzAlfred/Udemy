5c45993f2ce234cf4b13d3e78f8d49da7dc86d3a
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-10-01 16:19:35 -0500

    Feedback React

--- React/React Intermediate/01-intro-feedback/src/App.js
+++ React/React Intermediate/01-intro-feedback/src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const ChildrenComponent = ({thing}) => {
+
+     return (
+          <div>
+               <h4>Type a fruit: {thing}</h4>
+          </div>
+     );
+}
+
 function App() {
   return (
     <div className="App">
@@ -8,7 +17,7 @@ function App() {
            <h1>Feedback with React 🙂</h1>
         <img src={logo} className="App-logo" alt="logo" />
 
-        
+          <ChildrenComponent thing="Orage" />        
       </main>
     </div>
   );
