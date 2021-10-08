b916dda80e53c0061cba6922735e98f0497501cb
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-10-08 14:50:37 -0500

    Practising React

--- React/React Intermediate/02-practising-react/src/App.js
+++ React/React Intermediate/02-practising-react/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+     
+     const [email, setEmail] = useState("");
+     const [password, setPassword] = useState("");
+
+     const handleEmailChange = (e) => setEmail(e.target.value);
+
+     // console.log(email);
+
+     const handlePasswordChange = ( { target : { value }}) => setPassword(value);
+
+     // console.log(password);
+
+     const handleFormSubmit = (e) => {
+          console.log("Sending...");
+          console.log(e);
+          e.preventDefault();
+          alert(`User: ${email}, Password: ${password}`)
+     }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+         
+         <form onSubmit={ handleFormSubmit }>
+              <h2>Log In 😃</h2>
+
+              <label htmlFor="email">e-mail: </label>
+              <input id="email" type="email" value={email} onChange={ handleEmailChange } />
+
+              <label htmlFor="password">Password: </label>
+              <input id="password" type="password" value={password} onChange={ handlePasswordChange } />
+
+              <button type="submit">Send</button>
+         </form>
     </div>
   );
 }
