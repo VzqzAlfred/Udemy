e63a204758a128a99a6add41a332ab9c12427283
VzqzAlfred <alfred.vzqz96@outlook.com>
2022-05-14 20:07:31 -0500

    Redux

---
+++ React/React Intermediate/08-redux/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Redux</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
