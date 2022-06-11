345764db45a4ead6fec78669aa76e6a7673a1375
VzqzAlfred <alfred.vzqz96@outlook.com>
2022-06-11 17:51:13 -0500

    Tailwind with Redux

--- React/React Intermediate/09-css-redux/src/Views/Home/index.jsx
+++ React/React Intermediate/09-css-redux/src/Views/Home/index.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Chairs from "../../assets/chairs.png";
 
 const Home = () => {
-  return <div className="text-3xl font-bold">Home</div>;
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <div className="w-2/5">
+        <img className="w-full h-full object-cover" src={Chairs} alt="Chairs" />
+      </div>
+
+      <div className="w-3/5 flex flex-col justify-center items-center px-10">
+        <h2 className="text-lato xl font-bold font-lato">
+          Search your movie...
+        </h2>
+        <input className="bg-special-gray w-full my-3 h-9 p-1 border focus:outline-none focus:ring-2 focus:ring-gray-500 rounded" />
+        <div className="flex w-full justify-between">
+          <button
+            className="bg-red-500 hover:bg-red-600 text-white font-lato w-full shadow-lg h-9"
+            style={{ width: "48%" }}
+          >
+            Search
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-600 text-white font-lato w-full shadow-lg h-9"
+            style={{ width: "48%" }}
+          >
+            Clean
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- React/React Intermediate/09-css-redux/tailwind.config.js
+++ React/React Intermediate/09-css-redux/tailwind.config.js
@@ -1,7 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      lato: ["Lato", "sans-serif"], // font-lato
+    },
+    colors: {
+      "special-gray": "#F5F0F0",
+      ...defaultTheme.colors,
+    },
     extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["hover"],
+    },
   },
   plugins: [],
 };
