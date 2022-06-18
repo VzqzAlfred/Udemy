0706fb9a459cb43c665570ee4c73cab8ad23ef9f
VzqzAlfred <alfred.vzqz96@outlook.com>
2022-06-17 19:12:19 -0500

    Tailwind with Redux

--- React/React Intermediate/09-css-redux/src/Views/Results/index.jsx
+++ React/React Intermediate/09-css-redux/src/Views/Results/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import movieImages from "../../assets/movie-theater.png";
 import { useFetchMoviesQuery } from "../../Redux/API/Movies";
