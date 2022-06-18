59ef8384dc05fc87b5a860685a6dbb546035eada
VzqzAlfred <alfred.vzqz96@outlook.com>
2022-06-17 19:10:51 -0500

    Taildwind with Redux

---
+++ React/React Intermediate/09-css-redux/src/Views/Results/Components/ListItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ListItem = ({ id, image, title }) => {
+  return (
+    <div
+      key={id}
+      className="flex flew-row w-full mt-16"
+      style={{ minWidth: 650 }}
+    >
+      <div className="w-1/6">
+        <img src={image?.url} alt={title} className="w-32" />
+      </div>
+      <div className="w-5/6 flex flex-col items-start py-1 px-4">
+        <p className="font-lato text-3xl">{title ?? "No title"}</p>
+        <div className="flex h-full items-end">
+          <button
+            className="border-2 border-red-400 rounded-md w-48 h-10 text-xl text-red hover:bg-red-400 hover:text-white"
+            style={{ borderColor: "#f87171", color: "#f87171" }}
+          >
+            See more...
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
--- React/React Intermediate/09-css-redux/src/Views/Results/index.jsx
+++ React/React Intermediate/09-css-redux/src/Views/Results/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import movieImages from "../../assets/movie-theater.png";
 import { useFetchMoviesQuery } from "../../Redux/API/Movies";
+import List from "./Components/List";
 import Loading from "./Components/Loading";
 
 const Results = () => {
@@ -17,9 +18,12 @@ const Results = () => {
   return (
     <div className="flex flex-row w-full">
       <div className="w-3/5 h-screen overflow-y-auto pax-10">
-        {isLoading && isFetching && <Loading />}
+        {isLoading && isFetching ? (
+          <Loading />
+        ) : (
+          <List data={movies?.results} />
+        )}
       </div>
-
       <div className="w-2/5">
         <img
           src={movieImages}
