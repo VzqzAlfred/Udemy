732c9e385b2c6bbfc553fc0bcbd357c1f66aa9e7
VzqzAlfred <alfred.vzqz96@outlook.com>
2022-08-20 17:43:49 -0500

    Firebase Authentication

--- React/React-with-firebase/03-firebase/src/Components/Login.tsx
+++ React/React-with-firebase/03-firebase/src/Components/Login.tsx
@@ -1,43 +1,84 @@
 import React, { useState } from "react";
 import { auth } from "../firebaseconfig";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+import "react-toastify/dist/ReactToastify.css";
+
+const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+
   const SignInUser = (e: any) => {
     e.preventDefault();
-    try {
-      auth.createUserWithEmailAndPassword(email, password);
-      alert("User registrado");
-    } catch (error) {
-      console.log(error);
-    }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((r) => {
+        navigate("/");
+        return toast.success("Usuario registrado con éxito");
+      })
+      .catch((e) => {
+        if (e.code === "auth/invalid-email") {
+          return toast.error("El email no es correcto");
+        }
+        if (e.code === "auth/weak-password") {
+          return toast.error("La contraseña debe tener al menos 6 caracteres");
+        }
+      });
+  };
+
+  const LogInUser = (e: any) => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((res) => {
+        toast.success("Has iniciado sesión");
+        navigate("/");
+      })
+      .catch((e: any) => {
+        if (e.code === "auth/user-not-found") {
+          toast.error("El correo del usuario es incorrecta.");
+        }
+        if (e.code === "auth/wrong-password") {
+          toast.error("La contraseña del usuario es incorrecta.");
+        }
+      });
   };
 
   return (
-    <div className="row mt-5">
-      <div className="col"></div>
-      <div className="col">
-        <form onSubmit={(e) => SignInUser(e)} className="form-group">
-          <input
-            className="form-control"
-            placeholder="E-mail"
-            type="text"
-            onChange={({ target: { value } }) => setEmail(value)}
-          />
-          <input
-            onChange={({ target: { value } }) => setPassword(value)}
-            className="form-control mt-4"
-            placeholder="Password"
-            type="password"
-          />
-          <input className="btn btn-dark mt-4" type="submit" value="Log In" />
-        </form>
+    <>
+      <div className="row mt-5">
+        <div className="col"></div>
+        <div className="col">
+          <form onSubmit={(e) => SignInUser(e)} className="form-group">
+            <input
+              className="form-control"
+              placeholder="E-mail"
+              type="text"
+              onChange={({ target: { value } }) => setEmail(value)}
+            />
+            <input
+              onChange={({ target: { value } }) => setPassword(value)}
+              className="form-control mt-4"
+              placeholder="Password"
+              type="password"
+            />
+            <input
+              className="btn btn-dark mt-4"
+              type="submit"
+              value="Sign In"
+            />
+          </form>
+          <button className="btn btn-success mt-3" onClick={LogInUser}>
+            Log In
+          </button>
+          <Toaster />
+        </div>
+        <div className="col"></div>
       </div>
-      <div className="col"></div>
-    </div>
+    </>
   );
 };
 
-export default Login;
+export default SignIn;
--- React/React-with-firebase/03-firebase/src/Components/Menu.tsx
+++ React/React-with-firebase/03-firebase/src/Components/Menu.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebaseconfig";
 
 const Menu = () => {
+  const [usuario, setUsuario] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user: any) => {
+      if (user) {
+        setUsuario(user.email);
+      }
+    });
+  }, []);
+
+  const logOut = () => {
+    auth.signOut();
+    setUsuario(null);
+    navigate("/login");
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -12,16 +30,31 @@ const Menu = () => {
             </Link>
           </li>
           <li>
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
+            {!usuario ? (
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            ) : (
+              <span></span>
+            )}
           </li>
           <li>
-            <Link className="nav-link" to="/admin">
-              Admin
-            </Link>
+            {usuario ? (
+              <Link className="nav-link" to="/admin">
+                Admin
+              </Link>
+            ) : (
+              <span></span>
+            )}
           </li>
         </ul>
+        {usuario ? (
+          <button className="btn btn-danger" onClick={() => logOut()}>
+            Log Out
+          </button>
+        ) : (
+          <span></span>
+        )}
       </nav>
     </div>
   );
