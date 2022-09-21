b3d411b9a14c2ac77876ada69566d76726ece8a4
VzqzAlfred <alfred.vzqz96@outlook.com>
2022-09-21 16:30:32 -0500

    First Steps with Angular

--- Angular/first-app/src/app/app.component.ts
+++ Angular/first-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'My-first-App-in-Angular';
+  name = "HEY"
 }
